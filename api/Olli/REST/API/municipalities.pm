b3f51cc9e7e3dde366e07d393fe7b2b905081337
David Christensen <David.A.Christensen@gmail.com>
2017-01-13 15:47:50 -0600

    working api->database

--- api/Olli/REST/API/municipalities.pm
+++ api/Olli/REST/API/municipalities.pm
@@ -2,9 +2,12 @@ package Olli::REST::API::municipalities ;
 use warnings ;
 use strict ;
 use base qw/Apache2::REST::Handler/;
+use DBI;
+#use JSON;
+
 
 # Implement the GET HTTP method.
-sub GET{
+sub GET_dummy{
     my ($self, $request, $response) = @_ ;
     # dummy data
     my @muns = (
@@ -16,6 +19,34 @@ sub GET{
     $response->data()->{'municipalities'} = \@muns;
     return Apache2::Const::HTTP_OK ;
 }
+
+# Implement the GET HTTP method.
+sub GET {
+    my ($self, $request, $response) = @_ ;
+
+    # NOTE: "rld" should now be "olli" !!!
+    my $dbh = DBI->connect("dbi:Pg:database=rld;host=localhost;port=5432",
+			   "rld",
+			   "rld",
+			   {AutoCommit => 1, 
+			    RaiseError => 1, 
+			    PrintError => 0,
+			   }
+	) or die $DBI::errstr;
+    
+    $dbh->do("SET TIMEZONE='America/Winnipeg'");
+
+    my $SQL = "select id, year, name, population, is_bilingual, is_northern from municipalities order by year, name";
+    my $aref = $dbh->selectall_arrayref($SQL, { Slice => {} });
+    $dbh->disconnect;
+    
+#    my $json =  to_json( { municipalities => $aref } );
+
+    $response->data()->{'api_mess'} = 'Hello, this is Olli REST API' ;
+    $response->data()->{'municipalities'} = $aref;
+    return Apache2::Const::HTTP_OK ;
+}
+
 # Authorize the GET method.
 sub isAuth{
     my ($self, $method, $req) = @ _; 
