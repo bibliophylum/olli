a16362e859d248c9935ac268e021fdc30431e79d
David Christensen <David.A.Christensen@gmail.com>
2017-05-15 16:09:44 -0500

    thinner map lines and different map region colours

--- app/js/olliMap.js
+++ app/js/olliMap.js
@@ -6,10 +6,51 @@ $( document ).ready(function() {
     // Copy the point from the geojson file, but switch them here:
     var mymap = L.map('mapid').setView([ 50.3021654, -98.0323209 ], 6);  // coords of Portage la Prairie, from munis.json
 
-    L.geoJSON(geoMunData).addTo(mymap);
-    L.geoJSON(geoProvBoundary).addTo(mymap);
-    L.geoJSON(geoSimpleWater).addTo(mymap);
+    L.geoJSON(geoMunData, {
+	style: function (feature) { // Style option
+	    return {
+		'weight': 1,
+		'color': 'black',
+		'fillColor': 'yellow'
+	    }
+	}
+    }).addTo(mymap);
+    
+    L.geoJSON(geoProvBoundary, {
+	style: function (feature) {
+	    return {
+		'weight': 1,
+		'color': 'black'
+	    }
+	}
+    }).addTo(mymap);
+    
+    L.geoJSON(geoSimpleWater, {
+	style: function (feature) {
+	    return {
+		'weight': 1,
+		'color': 'navy',
+		'fillColor': 'blue'
+	    }
+	}
+    }).addTo(mymap);
 //    L.geoJSON(geoRoads).addTo(mymap);
     
 });
 
+/*
+From http://stackoverflow.com/questions/28339414/leaflet-change-map-color:
+
+$.getJSON('world.geo.json', function (geojson) { // load file
+    L.geoJson(geojson, { // initialize layer with data
+        style: function (feature) { // Style option
+            return {
+                'weight': 1,
+                'color': 'black',
+                'fillColor': 'yellow'
+            }
+        }
+    }).addTo(map); // Add layer to map
+});
+
+*/
