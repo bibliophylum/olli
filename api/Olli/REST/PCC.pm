5f3000e372ed6bac2044caec31292fadcc6f7d7d
MShumay <mako@wcgwave.ca>
2017-07-11 15:47:07 -0500

    PCC's second version added

--- api/Olli/REST/PCC.pm
+++ api/Olli/REST/PCC.pm
@@ -9,17 +9,19 @@
 	V01 (2017.07.11):
 		Currently, the pair is strictly branches.active_memberships and municipalities.population.
 		Later versions will easily allow arbitrary pairings, along with the automatic processing of multiple pairings.
+	
+	V02 (2017.07.11):
+		Altered to be much more robust.
+		Mostly allows arbitrary choice of pairings through the specification of the tables and the two fields.
+		Foreign keys for joins still need to be elegantly dealt with to allow fully automated PCC calculations.
 =end comment
 =cut
 
-package Olli::REST::API::testQuery ;
+package Olli::REST::PCC;
 use warnings ;
 use strict;
 use base qw/Apache2::REST::Handler/;
 use DBI;
-#use JSON;
-use Data::Dumper;
-#print Dumper(\%args);
 use List::MoreUtils qw(firstidx);
 
 
@@ -37,59 +39,60 @@ my $dbh = DBI->connect($dsn,
 			   }
 	) or die $DBI::errstr;
 
-#my $aref = $dbh->selectall_arrayref($SQL);#, { Slice => [] });
-
 my $SQL;
 my $sth;
 
-# Obtains array reference to entire table branches
-$SQL = "SELECT * FROM branches";
-$sth = $dbh->prepare($SQL) or die "Prepare exception: $DBI::errstr!";
-$sth->execute() or die "Execute exception: $DBI::errstr";
-my $branchesRes = $sth->fetchall_arrayref();
-my $branchesNames = $sth->{NAME};
+my $xt = 'branches';
+my $xfield = 'active_memberships';
+my $xfieldVal;
+my $xfieldSum = 0;
+my $xfieldAvg = 0;
+my $xfieldSD = 0;
+my @xfieldArr;
 
-my $branchesRows = @$branchesRes;
-my $branchesCols = @{$branchesRes->[0]};
-#print "branchesRows = $branchesRows\n";
-#print "branchesCols = $branchesCols\n";
-
-# Obtains array reference to entire table municipalities
-$SQL = "SELECT * FROM municipalities";
-$sth = $dbh->prepare($SQL) or die "Prepare exception: $DBI::errstr!";
-$sth->execute() or die "Execute exception: $DBI::errstr";
-my $munRes = $sth->fetchall_arrayref();
-my $munNames = $sth->{NAME};
-
-my $munRows = @$munRes;
-my $munCols = @{$munRes->[0]};
-#print "\nmunRows = $munRows\n";
-#print "munCols = $munCols\n";
-
-my @branchesVals;
-my @branchesPercentMember;
-my $floorSum = 0;
-my $memberSum = 0;
-my $floor;
-my $member;
-my $pop;
-
-my $percentMemberSum = 0;
-my $percentMemberNum = 0;
-my $percentMemberAvg;
-
-my @branchesValidTuples;
-my $branchesNumValidTuples = 0;
+my $yt = 'municipalities';
+my $yfield = 'population';
+my $yfieldVal;
+my $yfieldSum = 0;
+my $yfieldAvg = 0;
+my $yfieldSD = 0;
+my @yfieldArr;
 
 # Filter on the year of branch tuples
 my $validYear = 2015;
 
-my $branchesIDIdx = firstidx { $_ eq 'id' } @$branchesNames;
-my $branchesYearIdx = firstidx { $_ eq 'year' } @$branchesNames;
-my $branchesMunIDIdx = firstidx { $_ eq 'municipality_id' } @$branchesNames;
-my $branchesMemIdx = firstidx { $_ eq 'active_memberships' } @$branchesNames;
+# Obtains array reference to join of $xt and $yt
+$SQL = "SELECT $xfield as x_$xfield, $xt.year as x_year, $xt.id as x_id, $yt.* FROM $xt INNER JOIN $yt on $xt.municipality_id = $yt.id";
+$sth = $dbh->prepare($SQL) or die "Prepare exception: $DBI::errstr!";
+$sth->execute() or die "Execute exception: $DBI::errstr";
+my $res = $sth->fetchall_arrayref();
+my $resNames = $sth->{NAME};
 
-for (my $n = 0; $n < $branchesRows; $n++){
+# for(my $n = 0; $n < @$resNames; $n++){
+# 	print $resNames->[$n] . "\n";
+# }
+
+my $resRows = @$res;
+my $resCols = @{$res->[0]};
+# print "\nresRows = $resRows\n";
+# print "resCols = $resCols\n";
+
+my @resValidTuples;
+my $resNumValidTuples = 0;
+
+my $res_xtIDIdx = firstidx { $_ eq 'x_id' } @$resNames;
+my $res_xfieldIdx = firstidx { $_ eq 'x_' . $xfield } @$resNames;
+my $res_xtYearIdx = firstidx { $_ eq 'x_year' } @$resNames;
+my $res_ytIDIdx = firstidx { $_ eq 'id' } @$resNames;
+my $res_yfieldIdx = firstidx { $_ eq $yfield} @$resNames;
+
+# print $res_xtIDIdx . "\n";
+# print $res_xfieldIdx . "\n";
+# print $res_xtYearIdx . "\n";
+# print $res_ytIDIdx . "\n";
+# print $res_yfieldIdx . "\n";
+
+for (my $n = 0; $n < $resRows; $n++){
 	#$member = $branchesRes->[$n][9];
 	#$pop = $munRes->[$n][3];
 	#print "n = $n\n";
@@ -97,80 +100,68 @@ for (my $n = 0; $n < $branchesRows; $n++){
 	#print "Current branch = $branch\n";
 
 	# Notes which tuples hold valid values and won't skew output due to errors
-	if ($branchesRes->[$n][$branchesYearIdx] == $validYear && defined $branchesRes->[$n][$branchesMunIDIdx] && $branchesRes->[$n][$branchesMemIdx] > 0){# && $branchesRes->[$n][branchesIDIdx] != 842){
-		$branchesValidTuples[$n] = 1;
-		$branchesNumValidTuples++;
+	if ($res->[$n][$res_xtYearIdx] == $validYear && defined $res->[$n][$res_ytIDIdx] && $res->[$n][$res_xfieldIdx] > 0 && $res->[$n][$res_yfieldIdx] > 0){# && $branchesRes->[$n][branchesIDIdx] != 842){
+		$resValidTuples[$n] = 1;
+		$resNumValidTuples++;
+		#print "Defined\n";
 	}
 	else{
 		#print "Undefined.\n";
-		$branchesValidTuples[$n] = 0;
+		$resValidTuples[$n] = 0;
 	}
 
 }
 
-my $branchesMemberSum = 0;
-my $branchesMemberAvg;
-my @branchesMemberArr;
-
-my $branchesPopSum = 0;
-my $branchesPopAvg;
-my @branchesPopArr;
-my $members = 0;
-
-# Sums active_memberships and appropriate population values for later averaging
-for (my $n = 0; $n < $branchesRows; $n++){
-	if($branchesValidTuples[$n]){
-		$branchesMemberArr[$n] = $branchesRes->[$n][9];
-		$branchesMemberSum += $branchesMemberArr[$n];
+# Sums $xfield and appropriate $yfield values for later averaging
+for (my $n = 0; $n < $resRows; $n++){
+	if($resValidTuples[$n]){
+		$xfieldArr[$n] = $res->[$n][$res_xfieldIdx];
+		$xfieldSum += $xfieldArr[$n];
 		
-		$SQL = "SELECT population FROM municipalities where id=$branchesRes->[$n][$branchesMunIDIdx]";
-		$sth = $dbh->prepare($SQL) or die "Prepare exception: $DBI::errstr!";
-		$sth->execute() or die "Execute exception: $DBI::errstr";
-		$branchesPopArr[$n] = $sth->fetchrow_arrayref()->[0];
-		$branchesPopSum += $branchesPopArr[$n];
+		$yfieldArr[$n] = $res->[$n][$res_yfieldIdx];
+		$yfieldSum += $yfieldArr[$n];
 
-		#print "branch: $branchesRes->[$n][0],\tpop: $branchesPopArr[$n],\tmembers: $branchesMemberArr[$n]\n";
+		#print "x_id: $res->[$n][$res_xtIDIdx],\t$xfield: $xfieldArr[$n],\t$yfield: $yfieldArr[$n]\n";
 	}
 }
 
-$branchesMemberAvg = $branchesMemberSum/$branchesNumValidTuples;
-$branchesPopAvg = $branchesPopSum/$branchesNumValidTuples;
-print "\nNumber valid tuples: $branchesNumValidTuples\n";
-print "branchesMemberAvg = $branchesMemberAvg\n";
-print "branchesPopAvg = $branchesPopAvg\n\n";
 
-# Computation of Standard Deviations of both active_memberships and appropriate populations
-my $branchesMemberSD = 0;
-my $branchesPopSD = 0;
+$xfieldAvg = $xfieldSum/$resNumValidTuples;
+$yfieldAvg = $yfieldSum/$resNumValidTuples;
+print "\nNumber valid tuples: $resNumValidTuples\n";
+print $xfield . " = $xfieldAvg\n";
+print $yfield . "branchesPopAvg = $yfieldAvg\n\n";
 
-for (my $n = 0; $n < $branchesRows; $n++){
-	if($branchesValidTuples[$n]){
-		$branchesMemberSD += ($branchesMemberArr[$n]-$branchesMemberAvg)**2;
-		$branchesPopSD += ($branchesPopArr[$n]-$branchesPopAvg)**2;
+# Computation of Standard Deviations of both $xfield and appropriate $yfield
+
+for (my $n = 0; $n < $resRows; $n++){
+	if($resValidTuples[$n]){
+		$xfieldSD += ($xfieldArr[$n]-$xfieldAvg)**2;
+		$yfieldSD += ($yfieldArr[$n]-$yfieldAvg)**2;
 	}
 }
 
-$branchesMemberSD /= $branchesNumValidTuples;
-$branchesMemberSD = sqrt($branchesMemberSD);
+$xfieldSD /= $resNumValidTuples;
+$xfieldSD = sqrt($xfieldSD);
 
-$branchesPopSD /= $branchesNumValidTuples;
-$branchesPopSD = sqrt($branchesPopSD);
+$yfieldSD /= $resNumValidTuples;
+$yfieldSD = sqrt($yfieldSD);
 
-print "branchesMemberSD = $branchesMemberSD\n";
-print "branchesPopSD = $branchesPopSD\n";
+print "xfieldSD = $xfieldSD\n";
+print "yfieldSD = $yfieldSD\n";
 
 # Computation of Pearson's Correlation Coefficient
 my $PCC;
 my $PCCsum = 0;
-for (my $n = 0; $n < $branchesRows; $n++){
-	if($branchesValidTuples[$n]){
-		$PCC = (($branchesPopArr[$n]-$branchesPopAvg)*($branchesMemberArr[$n]-$branchesMemberAvg))/($branchesMemberSD*$branchesPopSD);
+for (my $n = 0; $n < $resRows; $n++){
+	if($resValidTuples[$n]){
+		$PCC = (($xfieldArr[$n]-$xfieldAvg)*($yfieldArr[$n]-$yfieldAvg))/($xfieldSD*$yfieldSD);
 		$PCCsum += $PCC;
 		#print "PCC = $PCC\n";
 		#<STDIN>
 	}
 }
-my $PCCavg = $PCCsum/$branchesNumValidTuples;
+my $PCCavg = $PCCsum/$resNumValidTuples;
 print "\nPCCavg = $PCCavg\n";
 
 print "\nEnd of script\n";
