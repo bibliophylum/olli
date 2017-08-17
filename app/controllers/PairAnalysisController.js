ef20179a6a9dac3a79d978fb8fb73a63e9c26f7c
MShumay <mako@wcgwave.ca>
2017-08-17 09:15:52 -0500

    Added filtering to pairAnalysis.html

--- app/controllers/PairAnalysisController.js
+++ app/controllers/PairAnalysisController.js
@@ -4,6 +4,7 @@
 olliApp.controller('PairAnalysisController', ['$scope', '$routeParams', 'pairAnalysisFactory', function ($scope, $routeParams, pairAnalysisFactory) {
     $scope.status = 'Waiting.';
     $scope.rawOutput = '';
+    $scope.filtered;
 
     $scope.compute = function(){
         $scope.status = "Calling for all pairs.";
@@ -11,9 +12,38 @@ olliApp.controller('PairAnalysisController', ['$scope', '$routeParams', 'pairAna
         pairAnalysisFactory.computeAllPairs()
             .then(function (response){
                 $scope.rawOutput = response.data.data.rawOutput;
+                $scope.filtered = $scope.rawOutput;
                 $scope.status = "Successful response.";
+
+                $scope.tableList = [];
+                $scope.fieldList = [];
+                var length = $scope.rawOutput[0].length;
+
+                for(var pairIdx = 0; pairIdx < length; pairIdx++){
+                    $scope.tableList.push($scope.rawOutput[0][pairIdx][0]);
+                    $scope.fieldList.push($scope.rawOutput[0][pairIdx][1]);
+                    $scope.fieldList.push($scope.rawOutput[0][pairIdx][3]);
+                }
+
+                $scope.tableList = $scope.tableList.unique();
+                $scope.fieldList = $scope.fieldList.unique();
             }, function (error){
                 $scope.status = "ERROR " + error.status;
             });
+    }
+
+    Array.prototype.contains = function(v) {
+        for(var i = 0; i < this.length; i++)
+            if(this[i] === v)  
+                return true;
+        return false;
+    };
+
+    Array.prototype.unique = function() {
+        var arr = [];
+        for(var i = 0; i < this.length; i++)
+            if(this[i] != '' && !arr.contains(this[i]))
+                arr.push(this[i]);
+        return arr; 
     }
 }]);
